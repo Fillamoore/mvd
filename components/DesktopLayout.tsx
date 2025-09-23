5ed935fc583f8a84bb6751a0285f726a47e65b4d
Fillamoore <colin.turner@newgamenewrules.com>
2025-09-23 23:46:45 +0100

    23/9 Architecture changes.

--- components/DesktopLayout.tsx
+++ components/DesktopLayout.tsx
@@ -5,7 +5,7 @@ import ScenarioPlayer from './ScenarioPlayer';
 export default function DesktopLayout() {
   return (
     <div className="hidden md:flex flex-col h-screen">
-      <div className="p-5 flex shadow-inner space-x-5 overflow-auto overflow-hidden bg-[url('/screen-background.jpg')] bg-cover bg-center">
+      <div className="p-5 flex shadow-inner space-x-5 overflow-auto bg-[url('/screen-background.jpg')] bg-cover bg-center">
         <aside className="desktop-sidebar-wrapper w-96">
           <DesktopSidebar />
         </aside>
--- components/DesktopSidebar.tsx
+++ components/DesktopSidebar.tsx
@@ -4,12 +4,9 @@
 import MasterView from './MasterView';
 
 export default function DesktopSidebar() {
-
   return (
-    
-    <div className="border-2 rounded-[10px] bg-gray-100 border-gray-800 h-full overflow-auto overflow-hidden">
-      <MasterView/>
+    <div className="border-2 rounded-[10px] bg-gray-100 border-gray-800 h-full overflow-hidden">
+      <MasterView />
     </div>
-
   );
 }
