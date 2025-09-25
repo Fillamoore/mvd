a282562e9a56a8d411b0dfb405ed3e9a2b5bb903
Fillamoore <colin.turner@newgamenewrules.com>
2025-09-25 14:43:46 +0100

    23/9 Module/Scenario update changes.

--- components/MobileMasterScreenFooter.tsx
+++ components/MobileMasterScreenFooter.tsx
@@ -1,38 +1,40 @@
 // components/MobileMasterScreenFooter.tsx
 'use client';
 
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
 export default function MobileMasterScreenFooter() {
   const pathname = usePathname();
-          {/*} style={{ WebkitTapHighlightColor: 'transparent' }} */}
+
+  const navigateTo = (path: string) => {
+    window.location.href = path;
+  };
 
   return (
-    <footer className="bg-black border-t border-gray-700 p-3">
-      <div className="flex justify-between items-center h-10">
-        {/* Master View Icon - ACTIVE */}
+    <footer className="bg-black border-t border-gray-700 px-3 py-2">
+      <div className="flex justify-around items-center h-10">
+        {/* Master View Icon */}
         <div
-          {/*href="/scenario-player" */}
-          onClick={() => window.location.href = '/scenario-player'}
-          className="flex items-center justify-center p-3 rounded-lg bg-transparent transition-colors select-none"
-          title="scenarios screen"
+          onClick={() => navigateTo('/mobile-master')}
+          className="flex items-center justify-center p-2 rounded-lg bg-transparent text-white cursor-pointer"
+          style={{ WebkitTapHighlightColor: 'transparent' }}
+          title="Master View"
         >
           <Image
-            src="/mobile-back-icon.png"
+            src="/master-icon.png"
             alt="Master Screen"
             width={24}
             height={24}
-            className="w6 h6"
+            className="object-contain"
           />
         </div>
 
-        {/* 
-        <Link
-          href="/scenario-player?module=1"
-          className="flex items-center justify-center p-2 rounded-lg text-gray-400 hover:text-white active:bg-transparent" 
-          style={{ WebkitTapHighlightColor: 'transparent' }} 
+        {/* Scenario Player Icon */}
+        <div
+          onClick={() => navigateTo('/mobile-master')}
+          className="flex items-center justify-center p-2 rounded-lg bg-transparent text-gray-400 cursor-pointer"
+          title="Scenario Player"
         >
           <div className="w-6 h-6 rounded flex items-center justify-center overflow-hidden">
             <Image
@@ -43,13 +45,13 @@ export default function MobileMasterScreenFooter() {
               className="object-contain"
             />
           </div>
-        </Link>
+        </div>
 
-        
-        <Link
-          href="/settings"
-          className="flex items-center justify-center p-2 rounded-lg text-gray-400 hover:text-white active:bg-transparent" 
-          style={{ WebkitTapHighlightColor: 'transparent' }} 
+        {/* Settings Icon */}
+        <div
+          onClick={() => navigateTo('/settings')}
+          className="flex items-center justify-center p-2 rounded-lg bg-transparent text-gray-400 cursor-pointer"
+          title="Settings"
         >
           <Image
             src="/settings-icon.png"
@@ -57,9 +59,8 @@ export default function MobileMasterScreenFooter() {
             width={24}
             height={24}
             className="object-contain"
-          />         
-        </Link>
-        */}
+          />
+        </div>
       </div>
     </footer>
   );
