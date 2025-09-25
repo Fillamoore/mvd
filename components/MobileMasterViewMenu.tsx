283e196cb5b6776be150b3afa494df80af7bb4b7
Fillamoore <colin.turner@newgamenewrules.com>
2025-09-25 15:51:39 +0100

    23/9 Module/Scenario update changes.

--- components/MobileMasterViewMenu.tsx
+++ components/MobileMasterViewMenu.tsx
@@ -3,6 +3,7 @@
 
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function MobileMasterViewMenu() {
   const router = useRouter();
@@ -15,8 +16,8 @@ export default function MobileMasterViewMenu() {
     <footer className="bg-black border-t border-gray-700 p-3 safe-area-padding-bottom">
       <div className="flex justify-start items-center">
         {/* Mobile Back Icon - TRANSPARENT */}
-        <button
-          onClick={handleBackClick}
+        <Link 
+          href = '/scenario-player'
           className="flex items-center justify-center p-3 rounded-lg bg-transparent transition-colors"
           title="Back to Scenarios"
         >
@@ -27,7 +28,7 @@ export default function MobileMasterViewMenu() {
             height={24}
             className="w-6 h-6" // Should be white icon
           />
-        </button>
+        </Link>
       </div>
     </footer>
   );
