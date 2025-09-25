d320b81c367e627b551b9ebe5e787c277b92d4ec
Fillamoore <colin.turner@newgamenewrules.com>
2025-09-25 12:07:56 +0100

    23/9 Module/Scenario update changes.

--- components/MobileLayout.tsx
+++ components/MobileLayout.tsx
@@ -1,4 +1,4 @@
-// components/MobileLayout.tsx - SIMPLIFIED
+// components/MobileLayout.tsx - SIMPLIFIED WITH DEBUG
 'use client';
 
 import { useState, useEffect } from 'react';
@@ -34,8 +34,8 @@ export default function MobileLayout() {
 
   if (!hydrated) {
     return (
-      <div className="flex flex-col h-screen">
-        <main className="flex-1 overflow-auto bg-gray-50">
+      <div className="flex flex-col h-screen border-4 border-purple-500"> {/* DEBUG */}
+        <main className="flex-1 overflow-auto bg-gray-50 border-4 border-yellow-500"> {/* DEBUG */}
           <div className="flex justify-center items-center h-full">Loading...</div>
         </main>
       </div>
@@ -43,17 +43,21 @@ export default function MobileLayout() {
   }
 
   return (
-    <div className="flex flex-col h-screen">
-      <main className="flex-1 overflow-auto bg-gray-50">
+    <div className="flex flex-col h-screen border-4 border-green-500 bg-green-100"> {/* DEBUG - MAKE VISIBLE */}
+      <main className="flex-1 overflow-auto bg-gray-50 border-4 border-blue-500 min-h-0"> {/* DEBUG */}
         <ScenarioPlayer />
       </main>
       
-      <MobileScenariosPlayerFooter
-        onReveal={handleReveal}
-        onNextScenario={handleNextScenario}
-        allRated={allRated}
-        isRevealed={isRevealed}
-      />
+      {/* DEBUG FOOTER CONTAINER - MAKE IT OBVIOUS */}
+      <div className="border-4 border-red-500 bg-red-300 p-1 z-50"> {/* DEBUG */}
+        <div className="text-red-600 font-bold text-center">FOOTER CONTAINER</div>
+        <MobileScenariosPlayerFooter
+          onReveal={handleReveal}
+          onNextScenario={handleNextScenario}
+          allRated={allRated}
+          isRevealed={isRevealed}
+        />
+      </div>
     </div>
   );
 }
